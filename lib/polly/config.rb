4324aaf019c550a0c86ce7283936118c59d69e82
Jon Bardin <diclophis@gmail.com>
2020-02-01 00:38:18 -0500

    allow external image repo

--- lib/polly/config.rb
+++ lib/polly/config.rb
@@ -9,5 +9,9 @@ module Polly
     def self.allowed_contexts
       rc["allowed_contexts"] || ["kubernetes-admin@kubernetes"]
     end
+
+    def self.image_repo
+      rc["image_repo"] || "polly-app:443"
+    end
   end
 end
