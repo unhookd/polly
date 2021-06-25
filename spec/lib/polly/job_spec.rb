e2c0edb720abb54f95f087095e78481ce8b71acd
Jon Bardin <diclophis@gmail.com>
2021-06-24 22:33:53 -0400

    repair spec

--- spec/lib/polly/job_spec.rb
+++ spec/lib/polly/job_spec.rb
@@ -25,8 +25,6 @@ describe Polly::Job do
 
   context "direct job creation" do
     it "allows jobs to be directly created" do
-      expect(false).to eq(true)
-
       expect(job.run_name).to eq(valid_run_name)
       expect(job.valid?).to eq(true)
 
