bda9007988ed7ca2472d89554b0cb062914bec45
Jon Bardin <diclophis@gmail.com>
2021-09-27 08:38:04 +0000

    bits

--- lib/polly/execute.rb
+++ lib/polly/execute.rb
@@ -133,16 +133,16 @@ module Polly
         end
       end
 
-      build_run_dir = "/polly/safe/run"
+      build_run_dir = Dir.mktmpdir #"/polly/safe/run"
       build_manifest_dir = File.join(build_run_dir, clean_name, current_revision)
       run_shell_path = File.join(build_manifest_dir, "run.sh")
 
       sleep_cmd_args = ["sleep", "infinity"]
 
       #####TODO: figure out fail modes run_cmd_args = ["bash", "-x", "-e", "-o", "pipefail", run_shell_path]
-      #if true #TODO: bits
-      #  run_cmd_args = ["bash", "-e", "-o", "pipefail", "-c", "bash #{run_shell_path} > /proc/1/fd/1 2> /proc/1/fd/2"]
-      #end
+      if true #TODO: bits
+        run_cmd_args = ["bash", "-e", "-o", "pipefail", "-c", "bash #{run_shell_path} > /proc/1/fd/1 2> /proc/1/fd/2"]
+      end
 
       #####TODO: better input for cmd: [] support
       ######run_cmd_args = ["bash", "-e", run_shell_path]
@@ -563,7 +563,7 @@ module Polly
           @runners.each { |job_namish, pod_name, cmd_io|
             if job_thang.run_name == job_namish
               unless jobs_to_keep_completed.include?(job_thang) || jobs_to_detach.include?(job_thang.run_name)
-                if false #TODO
+                if true #TODO
                   get_logs = ["kubectl", "logs", "-l", "name=#{pod_name}", "--all-containers=true"]
                   ########@all_exited = false
                   get_log_runners << [job_namish, "logs-#{pod_name}", execute_simple(:async, get_logs, {})]
