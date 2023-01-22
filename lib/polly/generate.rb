751f4f00e16e690bfe84e502b6668a2b2935ec7c
Jon Bardin <diclophis@gmail.com>
2023-01-22 06:32:18 +0000

    test rebootstrap consistent builds

--- lib/polly/generate.rb
+++ lib/polly/generate.rb
@@ -79,7 +79,7 @@ module Polly
         }
       end
 
-      def description(s) # adds annotation for github packages, bar
+      def description(s) # adds annotation for github packages
         command("LABEL") {
           "org.opencontainers.image.description #{s}"
         }
