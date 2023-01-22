0a5d96814228de6e8759e63bf994c66663ebcc82
Jon Bardin <diclophis@gmail.com>
2023-01-22 07:45:28 +0000

    Debug

--- lib/polly/generate.rb
+++ lib/polly/generate.rb
@@ -79,7 +79,7 @@ module Polly
         }
       end
 
-      def description(s) # adds annotation for github packages
+      def description(s) # adds annotation for github packages, etc
         command("LABEL") {
           "org.opencontainers.image.description #{s}"
         }
