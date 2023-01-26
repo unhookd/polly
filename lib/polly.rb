31e241465c2824f4496763774027638b82d809f0
Jon Bardin <diclophis@gmail.com>
2023-01-26 07:18:44 +0000

    add buildctl

--- lib/polly.rb
+++ lib/polly.rb
@@ -1,4 +1,4 @@
-if ENV["OUTER"]
+if Dir.exists?(File.expand_path("../.bundle", __dir__))
   ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
   require "bundler/setup" # Set up gems listed in the Gemfile.
