2905862250cfa9d508af420621a4d444f1176a77
Jon Bardin <diclophis@gmail.com>
2024-04-01 02:47:24 +0000

    even better branch name repair

--- lib/polly/execute.rb
+++ lib/polly/execute.rb
@@ -211,7 +211,7 @@ module Polly
           app = current_app
           #image_repo = Polly::Config.image_repo
 
-          "polly-registry:23443/polly-registry/#{app}:#{branch}-#{version}"
+          "polly-registry:23443/polly-registry/#{app}:#{version}"
         end
       end
 
